1b82b244480555bafb32fc71746b89a480088a59
Darrel Clute <darrel@darrelclute.net>
2015-01-15 08:30:11 -0700

    Make gpg-agent-info sourcing more robust
    Signed-off-by: Darrel Clute <darrel@darrelclute.net>

--- bash/.config/bash/profile.d/gpg-agent.sh
+++ bash/.config/bash/profile.d/gpg-agent.sh
@@ -1,11 +1,11 @@
-if [ -f $HOME/.gnupg/gpg-agent-info ] ; then
-    . $HOME/.gnupg/gpg-agent-info
-elif [ -f $HOME/.gnupg/gpg-agent-info-$(hostname) ] ; then
-    . $HOME/.gnupg/gpg-agent-info-$(hostname)
-elif [ -f $HOME/.gpg-agent-info ] ; then
-    . $HOME/.gpg-agent-info
+if [ $(pgrep -q -P $(grep GPG_AGENT_INFO ${HOME}/.gnupg/gpg-agent-info | awk -F":" '{print $2}') >/dev/null 2>&1) ] ; then
+  . ${HOME}/.gnupg/gpg-agent-info
+elif [ $(pgrep -q -P $(grep GPG_AGENT_INFO ${HOME}/.gnupg/gpg-agent-info-$(hostname) | awk -F":" '{print $2}') >/dev/null 2>&1) ] ; then
+  . ${HOME}/.gnupg/gpg-agent-info-$(hostname)
+elif [ $(pgrep -q -P $(grep GPG_AGENT_INFO ${HOME}/.gpg-agent-info | awk -F":" '{print $2}') >/dev/null 2>&1) ] ; then
+  . ${HOME}/.gpg-agent-info
 else
-    eval $(gpg-agent --daemon --sh --write-env-file=~/.gnupg/gpg-agent-info)
+  eval $(gpg-agent --daemon --sh --write-env-file=${HOME}/.gnupg/gpg-agent-info)
 fi
 
 
