335ddb8368213f44f5aa6a6544e7adf371286332
Darrel Clute <darrel@darrelclute.net>
2015-02-05 12:39:47 -0700

    Make Mac awareness for ls deterministic

--- bash/.config/bash/profile.d/colorls.sh
+++ bash/.config/bash/profile.d/colorls.sh
@@ -7,23 +7,23 @@ case "$LC_ALL$LC_CTYPE" in
 esac
 export LS_OPTIONS
 
-if [ dircolors 1>/dev/null 2>&1 ] ; then
-    alias dircolors='dircolors -b'
-elif [ gdircolors 1>/dev/null 2>&1 ] ; then
+if [ -f /opt/local/bin/gdircolors ] ; then
     alias dircolors='gdircolors -b'
+elif [ -f /usr/bin/dircolors ] ; then
+    alias dircolors='dircolors -b'
 fi
 
 if [ -f "$HOME/.config/bash/DIR_COLORS" ] ; then
     eval $(dircolors $HOME/.config/bash/DIR_COLORS)
 elif [ -f "$HOME/.dircolors" ] ; then
-    eval $(dircolors -b $HOME/.dircolors)
+    eval $(dircolors $HOME/.dircolors)
 elif [ -f "/etc/DIR_COLORS" ] ; then
-    eval $(dircolors -b /etc/DIR_COLORS)
+    eval $(dircolors /etc/DIR_COLORS)
 else
-    eval $(dircolors -b)
+    eval $(dircolors)
 fi
 
-if [ gls 1>/dev/null 2>&1 ] ; then
+if [ -f /opt/local/bin/gls ] ; then
     alias ls='gls $LS_OPTIONS -GAh --color=auto'
 else
     alias ls='ls $LS_OPTIONS -GAh --color=auto'
