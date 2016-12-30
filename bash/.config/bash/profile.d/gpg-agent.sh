f144d25c674d3c19987a157ee8000bb0e74943ec
Darrel Clute <darrel@darrelclute.net>
2016-12-30 14:27:12 -0700

    Make finding a running gpg-agent more robust
    Signed-off-by: Darrel Clute <darrel@darrelclute.net>

--- bash/.config/bash/profile.d/gpg-agent.sh
+++ bash/.config/bash/profile.d/gpg-agent.sh
@@ -1,10 +1,11 @@
-if [ -S ~/.gnupg/S.gpg-agent.ssh ] ; then
-    export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
-elif [ -S ~/.gnupg/S.gpg-agent ] ; then
-    :
-elif [ $(command -v gpg-agent) ] ; then
-    gpg-agent
+if [ -z ${SSH_AGENT_SOCK+x} ] ; then
     if [ -S ~/.gnupg/S.gpg-agent.ssh ] ; then
         export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+    elif [ -S ${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh ] ; then
+        export SSH_AUTH_SOCK=${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh
+    elif [ -S ~/.gnupg/S.gpg-agent ] ; then
+        :
+    elif [ $(command -v gpg-agent) ] ; then
+        eval $(gpg-agent)
     fi
 fi
